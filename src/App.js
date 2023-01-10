5cd01ed23abdb99482455de36c248ce71cfb79d3
igor-veselkov <veselkov_igor@bk.ru>
2023-01-10 21:46:35 +0500

    add layout and pizza components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import './scss/app.scss';
+
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Пицца</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="400" />
+            <PizzaBlock title="Чизбургер-пицца" price="450" />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+
+          <h2 class="content__title">Роллы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="400" />
+            <PizzaBlock title="Чизбургер-пицца" price="450" />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+
+          <h2 class="content__title">Сеты роллов</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="400" />
+            <PizzaBlock title="Чизбургер-пицца" price="450" />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+
+          <h2 class="content__title">Напитки</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price="400" />
+            <PizzaBlock title="Чизбургер-пицца" price="450" />
+            <PizzaBlock />
+            <PizzaBlock />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,15 @@
+function Categories() {
+  return (
+    <div class="categories">
+      <ul>
+        <li class="active">Меню</li>
+        <li>Пицца</li>
+        <li>Роллы</li>
+        <li>Сеты роллов</li>
+        <li>Напитки</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
